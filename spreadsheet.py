27910dd80b2d6b2e1ef4b8bf0816a6a23ebcb677
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-11-18 17:53:13 -0700

    New files

---
+++ spreadsheet.py
@@ -0,0 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+pp = pprint.PrettyPrinter()
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('manager.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('GoogleSheet').sheet1
+#sheet = client.openall()
+sheet.append_row(['a', '123412', 'word'])
+# https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
+# https://www.youtube.com/watch?v=7I2s81TsCnc
+# sheet.delete_row(2)
+#data = sheet.get_all_records()
+# print(data)
