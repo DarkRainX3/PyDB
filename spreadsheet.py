4297f44fb41ecfe2b29f543c43feb516e01cf23f
Waley <44916399+DarkRainX3@users.noreply.github.com>
2018-11-18 22:48:00 -0700

    new func

--- spreadsheet.py
+++ spreadsheet.py
@@ -10,11 +10,29 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('manager.json', scope)
 client = gspread.authorize(creds)
 
-sheet = client.open('GoogleSheet').sheet1
+worksheet = client.open('GoogleSheet')
+sheet = worksheet.sheet1
+sheets = worksheet.worksheets()
+sheet2 = worksheet.worksheet('Sheet2')
 #sheet = client.openall()
-sheet.append_row(['a', '123412', 'word'])
+#sheet.append_row(['a', '8', 'f'])
 # https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
 # https://www.youtube.com/watch?v=7I2s81TsCnc
+# https://gspread.readthedocs.io/en/latest/user-guide.html#opening-a-spreadsheet
 # sheet.delete_row(2)
-#data = sheet.get_all_records()
-# print(data)
+# row = ["I'm", "inserting", "a", "row", "into", "a,", "Spreadsheet", "with", "Python"]
+# index = 1
+# sheet.insert_row(row, index)
+# sheet.delete_row(1)
+data = sheet.get_all_records()
+dataa = sheet.row_values(1)
+dataaa = sheet.col_values(1)
+cell = sheet.cell(1, 2)
+pp.pprint(data)
+print(dataa)
+print(dataaa)
+print(cell)
+print(sheet.row_count)
+print(sheet.row_values)
+print(sheets)
+print(sheet2.get_all_records())
